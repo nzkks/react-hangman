c6d36031334613fcb340b01c822be674ca0aeed6
Shanthosh Krishnakumar <kkshanthosh@gmail.com>
2024-01-29 20:01:09 +1300

    On lost status show the missed (not guessed) letters red when revealed

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -18,7 +18,14 @@ export function HangmanWord({ guessedLetters, wordToGuess, reveal = false }: Han
     >
       {wordToGuess.split('').map((letter, index) => (
         <span key={index} style={{ borderBottom: '.1em solid black' }}>
-          <span style={{ visibility: guessedLetters.includes(letter) || reveal ? 'visible' : 'hidden' }}>{letter}</span>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter) || reveal ? 'visible' : 'hidden',
+              color: !guessedLetters.includes(letter) && reveal ? 'red' : 'black'
+            }}
+          >
+            {letter}
+          </span>
         </span>
       ))}
     </div>
