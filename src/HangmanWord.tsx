fb0b40d7faa550bc0ae417e058bff8e2a468e8b7
Shanthosh Krishnakumar <kkshanthosh@gmail.com>
2024-01-29 15:01:17 +1300

    create the basic logic of the Hangman Word component

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,23 @@
 export function HangmanWord() {
-  return <div>Word</div>;
+  const word = 'test';
+  const guessedLetters = ['t', 'e', 'g'];
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        gap: '.25em',
+        fontSize: '6rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace'
+      }}
+    >
+      {word.split('').map((letter, index) => (
+        <span key={index} style={{ borderBottom: '.1em solid black' }}>
+          <span style={{ visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden' }}>{letter}</span>
+        </span>
+      ))}
+    </div>
+  );
 }
